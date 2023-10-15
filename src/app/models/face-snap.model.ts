7a28a01b8a2885c0a17839fcdaea6a31547a0116
soufiane kabad <kabadsoufiane@gmail.com>
2023-10-15 17:43:55 +0200

    ngfor-ngif

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -4,17 +4,20 @@ export class FaceSnap {
   createdDate: Date;
   snaps: number;
   imageUrl: string;
+  location?: string;
 
   constructor(  title: string,
                 description: string,
                 createdDate: Date,
                 snaps: number,
-                imageUrl: string)
+                imageUrl: string,
+                location?: string)
   {
     this.title = title;
     this.description = description;
     this.imageUrl = imageUrl;
     this.createdDate = createdDate;
     this.snaps = snaps;
+    this.location =  location;
   }
 }
